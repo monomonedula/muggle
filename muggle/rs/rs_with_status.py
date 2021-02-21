3cffa2b9ca771fd0777365a1f110b66d1c927a00
monomonedula <valh@tuta.io>
2021-02-21 18:15:13 +0200

    type hints

--- muggle/rs/rs_with_status.py
+++ muggle/rs/rs_with_status.py
@@ -8,11 +8,11 @@ from muggle.rs.rs_wrap import RsWrap
 class RsWithStatus(RsWrap):
     _codes = {code.value: code.phrase for code in HTTPStatus}
 
-    def __init__(self, status, reason: str = None, response: Response = None):
+    def __init__(self, status: int, reason: str = None, response: Response = None):
         if status < 100 or status > 999:
             raise TypeError("Bad status code: %r" % status)
-        self._status = status
-        self._reason = reason or self._codes[status]
+        self._status: int = status
+        self._reason: str = reason or self._codes[status]
         super(RsWithStatus, self).__init__(RsEmpty() if response is None else response)
 
     async def status(self) -> str:
